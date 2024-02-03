3bf53712d92188341f1423637891a219f61c1154
ncc-social <social@ncc.gov.gh>
2024-02-03 17:04:41 +0000

    wip: staff domain workspace

---
+++ erpnext15_custom/erpnext15_custom/doctype/xyz/xyz.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, NACOC and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("XYZ", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ erpnext15_custom/erpnext15_custom/doctype/xyz/xyz.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, NACOC and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class XYZ(Document):
+	pass
