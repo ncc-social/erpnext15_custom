1ac4858eb67faf4ff995734cb9e1f04479f16198
ncc-social <social@ncc.gov.gh>
2024-06-29 09:40:01 +0000

    feat: hide children - HR workspace

--- erpnext15_custom/public/js/custom/workspace.js
+++ erpnext15_custom/public/js/custom/workspace.js
@@ -1,7 +1,2 @@
-frappe.ui.form.on("Workspace", {
-    onload: frm => {
-        $('div[item-parent="HR"]').parent().hide();
-        $('a[title="HR"]').siblings('div').hide();
-    }
-});
-
+$('div[item-parent="HR"]').parent().hide();
+$('a[title="HR"]').siblings('div').hide();
